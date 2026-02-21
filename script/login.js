7ad3af753d101f5a6ec13652e51909e16f1e8eda
Md. Nur A Alam <mdnuralam2812@gmail.com>
2026-02-22 02:35:03 +0600

    creaded index.html and added all necessery functionalities

---
+++ script/login.js
@@ -0,0 +1,13 @@
+document.getElementById("login-btn").addEventListener("click", function(){
+    const mobileNumber = document.getElementById("mobile-number").value;
+    
+    const pin = document.getElementById("pin").value;
+
+    if (mobileNumber==="01317631378" && pin==="1234") {
+        window.location.assign("/home.html");
+    }
+    else{
+        alert("Please enter valid info")
+    }
+    console.log(mobileNumber+' '+pin);
+})
